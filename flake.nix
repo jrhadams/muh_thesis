a160f43aae4997291de48c44bd75074dd7cbd329
jrha <j.rand.adams@protonmail.com>
2025-02-28 11:47:44 +0100

    please ignore flake stuff

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A quarto flake with pdf";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/22b3e817a2916e4bbd745e141d7129cec1499fb5";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      rmark = pkgs.rPackages.buildRPackage {
+        name = "rix";
+        src = pkgs.fetchFromGitHub {
+          owner = "ropensci";
+          repo = "rix";
+          rev = "287e8bd5d41649247747a499e459ef33cc7c76e0";
+          sha256 = "0v4q9zqffv97nc4zda8db2jlgwws7pgqbq3pk4fn34zf50zb792d";
+        };
+        propagatedBuildInputs = with pkgs.rPackages; [bslib evaluate jsonlite knitr stringr tinytex yaml xfun data_table ggplot2 codetools curl sys R_utils];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [pkgs.bashInteractive];
+        buildInputs = with pkgs; [R rmark pandoc quarto texliveFull];
+      };
+    });
+}
